540798469d1f0cd034dab5b7a026d2f1a0285db7
Isabelle-Vicente <isabelle.v.evil@gmail.com>
2024-01-02 22:21:13 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, } from '@angular/core';
+import { DistritosService } from './service/distritos.service';
+
+
+interface Food {
+  value: string;
+  viewValue: string;
+}
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'tabela-api';
+
+export class AppComponent implements OnInit {
+  distritos: Array<{ id: number, nome: string }> = [];
+  displayedColumns: string[] = ['id', 'name'];
+  dataSource = this.distritos;
+
+
+  constructor(private distritosService: DistritosService) { }
+
+  ngOnInit() {
+    this.distritosService.listar().subscribe(
+      retornoApi => this.distritos = retornoApi
+    )
+  }
+};
+export class SelectOverviewExample {
+  foods: Food[] = [
+    {value: 'steak-0', viewValue: 'Steak'},
+    {value: 'pizza-1', viewValue: 'Pizza'},
+    {value: 'tacos-2', viewValue: 'Tacos'},
+  ];
 }
+
+
+
+// export class AppComponent {
+//   title = 'tabela-api';
+// }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {MatTableModule} from '@angular/material/table';
+import {FormsModule} from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule,
-    AppRoutingModule
+    CommonModule,
+    HttpClientModule,
+    MatTableModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatInputModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
